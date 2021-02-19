ff20b332ebdd7dc587ba140a329bc5372c4e8040
Vladimir Mironov <v.mironov@viseven.com>
2021-02-19 11:53:37 +0200

    lect60 - 3rd party middleware (morgan)

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,12 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,6 +23,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) ROUTE HANDLERS
 const getAllTours = (req, res) => {
   console.log(req.requesTime);
   res.status(200).json({
@@ -109,6 +115,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/V1/tours/:id', updateTour);
 // app.delete('/api/V1/tours/:id', deleteTour);
 
+// 3) ROUTES
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
@@ -116,6 +123,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) START THE SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
