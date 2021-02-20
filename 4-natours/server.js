7b113d11c47493499a813b0ea49c1ff1ccbf4eec
Vladimir Mironov <v.mironov@viseven.com>
2021-02-20 18:53:16 +0200

    lect 90 - updating docyments (nor workong properly

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 // console.log(process.env);
 
@@ -16,26 +16,11 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then((con) => {
     // console.log(con.connections);
     console.log('DB connection successful!');
   }); // подключаем ее
-
-// const testTour = new Tour({
-//   name: 'The Park Camper',
-//   price: 997,
-// });
-
-// testTour
-//   .save()
-//   .then((doc) => {
-//     console.log(doc);
-//   })
-//   .catch((err) => {
-//     console.log('ERRorr', err);
-//   });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
