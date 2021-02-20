658fba3a2d7fe1ec25ac3fac819414c161e99e65
Vladimir Mironov <v.mironov@viseven.com>
2021-02-20 18:12:02 +0200

    lect 89 - read documents from db

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -27,32 +27,39 @@ const Tour = require('./../models/tourModel');
 //   next();
 // };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requesTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requesTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  // console.log(req.params);
-  const id = req.params.id * 1;
-
-  // const tour = tours.find((el) => el.id === id);
-  // // console.log(tour.id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   // results: tours.length,
-  //   data: {
-  //     tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
