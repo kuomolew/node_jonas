38771154111f58f7cbb53d1cce763f65afb6c88b
Vladimir Mironov <v.mironov@viseven.com>
2021-03-21 22:18:55 +0200

    lect 44 async await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,18 +19,37 @@ const writeFilePromise = (file, data) => {
   });
 };
 
-readFilePromise(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed:`, data);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePromise('dog-img.txt', res.body.message);
-  })
-  .then(() => {
+
+    await writeFilePromise('dog-img.txt', res.body.message);
     console.log('Random dog image is saved!');
-  })
-  .catch((err) => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+getDogPic();
+
+// readFilePromise(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed:`, data);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePromise('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image is saved!');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
